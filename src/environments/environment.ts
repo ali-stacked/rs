5cd2817cf295781810c4c40e172a9ef4fdde0aef
ali-stacked <76439804+ali-stacked@users.noreply.github.com>
2020-12-26 05:02:45 -0500

    nav

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   weatherApiKey: 'aab52a2060a1fbb02b284f9922b0f2e5',
   weatherUrl: 'http://api.openweathermap.org/data/2.5',
   firebase : {
-    apiKey: 'AIzaSyCfuuM10IhiMDqK-IJiRc-dPFEjHcpcnR4',
-    authDomain: 'tennisgamelove-2703c.firebaseapp.com',
+    // apiKey: 'AIzaSyCfuuM10IhiMDqK-IJiRc-dPFEjHcpcnR4',
+    // authDomain: 'tennisgamelove-2703c.firebaseapp.com',
     databaseURL: 'https://tennisgamelove-2703c.firebaseio.com',
     projectId: 'tennisgamelove-2703c',
     storageBucket: 'tennisgamelove-2703c.appspot.com',
